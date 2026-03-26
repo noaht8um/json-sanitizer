22ed38fae87a452e4b048d05de3c1a5fb0cda95c
Noah Tatum <me@ntatum.com>
2026-03-26 19:51:38 -0400

    Remove @cloudflare/vite-plugin dependency and update vite.config.ts to exclude it from plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss()],
 });
